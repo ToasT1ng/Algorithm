a131abb1156ffeea097461bf84715b0b1b1c02fd
ToasT1ng <jrainboww9@gmail.com>
2022-02-16 23:25:23 +0900

    Update 예상 대진표.java

--- programmers/예상 대진표.java
+++ programmers/예상 대진표.java
@@ -2,7 +2,6 @@ class Solution
 {
     public int solution(int n, int a, int b)
     {
-        int answer = 0;
         int round = 0;
         while (true) {
             round++;
@@ -14,7 +13,6 @@ class Solution
             a = a/2 + a%2;
             b = b/2 + b%2;
                 
-            n = n/2;
         }
 
         return round;
