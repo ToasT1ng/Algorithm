592771cd6fbe103544502dda8ac63394644d0942
ToasT1ng <jrainboww9@gmail.com>
2022-02-23 02:06:32 +0900

    Need to fix test case error

---
+++ programmers/실패율.java
@@ -0,0 +1,56 @@
+// https://programmers.co.kr/learn/courses/30/lessons/42889#
+
+import java.util.*;
+
+class Failure implements Comparable<Failure>{
+    public double failure;
+    public int position;
+
+    public Failure(double failure, int position) {
+        this.failure = failure;
+        this.position = position;
+    }
+    
+    @Override
+    public int compareTo(Failure target) {
+        return this.failure > target.failure ? -1 : 1;
+    }
+
+    @Override
+    public String toString(){
+        return failure + " / " + position;
+    }
+}
+
+class Solution {
+    public int[] solution(int N, int[] stages) {
+        int[] answer = new int[N];
+        int[] player = new int[N+1];
+        int[] notClear = new int[N+1];
+        for (int stage : stages) {
+            if (stage == N+1) {
+                stage = N;
+            } else {
+                notClear[stage] += 1;
+            }
+            for (int j=1 ; j<stage ; j++) {
+                player[j] += 1;
+            }
+        }
+        
+        List<Failure> failureList = new ArrayList<>();
+        for (int i=1 ; i<N+1 ; i++) {
+            double currentFailure = (double) notClear[i] / player[i];
+            failureList.add(new Failure(currentFailure, i));
+        }
+        Collections.sort(failureList);
+        
+        int index=0;
+        for (Failure each : failureList) {
+            answer[index] = each.position;
+            index++;
+        }
+        
+        return answer;
+    }
+}
