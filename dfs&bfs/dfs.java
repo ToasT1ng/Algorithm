e0c833ad29a42f53d3438c1f58ba8cfb581c6553
ToasT1ng <jrainboww9@gmail.com>
2022-03-10 22:58:46 +0900

    Create dfs.java

---
+++ dfs&bfs/dfs.java
@@ -0,0 +1,75 @@
+import java.util.*;
+
+class Solution {
+	public void solution(int[] input) {
+		dfs_all(0, input, new boolean[input.length], "");
+		System.out.println("===============");
+		dfs_limit(0, input);
+		System.out.println("===============");
+	}
+
+	public void dfs_all(int index, int[] input, boolean[] visited, String result) {
+		if (index >= input.length) {
+			System.out.println(result);
+			return;
+		}
+		for (int i=0 ; i<input.length ; i++) {
+			if (!visited[i]) {
+				visited[i] = true;
+				int beforeLength = result.length();
+				result += String.valueOf(input[i]);
+				dfs_all(index+1, input, visited, result);
+				result = result.substring(0, beforeLength);
+				visited[i] = false;
+			}
+		}
+	}
+
+	// 0 1 2 3
+	// 0 1 3 2
+	// 0 2 1 3
+	// 0 2 3 1
+	// 0 3 1 2
+	// 0 3 2 1
+	// 1 2 3
+	// 1 3 2
+	// 2 3
+	// 3
+
+	public void dfs_limit(int index, int[] input) {
+		for (int i=0 ; i<input.length ; i++) {
+			boolean[] visited = new boolean[input.length];
+			for (int j=0 ; j<=i ; j++) {
+				visited[j] = true;
+			}
+			String result = String.valueOf(input[i]);
+			dfs_all2(i+1, input, visited, result);
+		}
+	}
+
+	public void dfs_all2(int index, int[] input, boolean[] visited, String result) {
+		if (index >= input.length) {
+			System.out.println(result);
+			return;
+		}
+		for (int i=0 ; i<input.length ; i++) {
+			if (!visited[i]) {
+				visited[i] = true;
+				int beforeLength = result.length();
+				result += String.valueOf(input[i]);
+				dfs_all2(index+1, input, visited, result);
+				result = result.substring(0, beforeLength);
+				visited[i] = false;
+			}
+		}
+	}
+
+}
+
+class RunClass {
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		int[] input = {0,1,2,3};
+		s.solution(input);
+	}
+}
