78c9f816f0c883c8d260ea6b5464917bc0d08f21
ToasT1ng <jrainboww9@gmail.com>
2022-03-24 00:58:00 +0900

    Update 단체사진 찍기.java

--- programmers/단체사진 찍기.java
+++ programmers/단체사진 찍기.java
@@ -1,61 +1,49 @@
-// https://programmers.co.kr/learn/courses/30/lessons/1835
-
-import java.util.*;
-
 class Solution {
-    int answer = 0;
     String[] friends = {"A", "C", "F", "J", "M", "N", "R", "T"};
-    boolean[] visited = new boolean[8];
-    List<String> sortingFriends = new ArrayList<>();
-    
-    public int solution(int n, String[] data) {
-        dfs(0, data);
+    int answer = 0;
+    public int solution(int n, String[] data) {  
+        dfs(0, data, new boolean[8], "");
         return answer;
     }
-    
-    public void dfs(int location, String[] data) {
-        System.out.println(location);
-        if (location == 8) {
-            check(data);
+
+    public void dfs(int index, String[] data, boolean[] visited, String result) {
+        if (index >= 8) {
+            if (isCorrect(data, result)) answer++;
             return;
         }
-        for (int i=0 ; i<8 ; i++) {
+        for (int i=0 ; i<visited.length ; i++) {
             if (!visited[i]) {
                 visited[i] = true;
-                sortingFriends.add(friends[i]);
-                dfs(location+1, data);
+                int originResultLength = result.length();
+                result += friends[i];
+
+                dfs(index+1, data, visited, result);
+
+                result = result.substring(0, originResultLength);
                 visited[i] = false;
-                sortingFriends.remove(location);
             }
         }
     }
-    
-    public void check(String[] data) {
-        for (String eachCommand : data) {
-            char firstFriend = eachCommand.charAt(0);
-            char secondFriend = eachCommand.charAt(2);
-            int firstFriendPos = sortingFriends.indexOf(String.valueOf(firstFriend));
-            int secondFriendPos = sortingFriends.indexOf(String.valueOf(secondFriend));
-            int realDistance = Math.abs(firstFriendPos - secondFriendPos) - 1;
+
+    public boolean isCorrect(String[] data, String result) {
+        for (String eachData : data) {
+            String firstFriend = String.valueOf(eachData.charAt(0));
+            String secondFriend = String.valueOf(eachData.charAt(2));
+            String comparator = String.valueOf(eachData.charAt(3));
+            int amount = eachData.charAt(4) - '0';
             
-            char sign = eachCommand.charAt(3);
-            int betweenNumber = eachCommand.charAt(4) - '0';
+            int firstFriendIndex = result.indexOf(firstFriend);
+            int secondFriendIndex = result.indexOf(secondFriend);
+            int betweenFriends = Math.abs(firstFriendIndex - secondFriendIndex) - 1;
             
-            if (sign == '=') {
-                if (realDistance != betweenNumber) {
-                    return;
-                }
-            } else if (sign == '<') {
-                if (realDistance >= betweenNumber) {
-                    return;
-                }
+            if (comparator.equals("=")) {
+                if (betweenFriends != amount) return false;
+            } else if (comparator.equals(">")) {
+                if (betweenFriends <= amount) return false;
             } else {
-                if (realDistance <= betweenNumber) {
-                    return;
-                }
+                if (betweenFriends >= amount) return false;
             }
-            
         }
-        answer++;
+        return true;
     }
 }
