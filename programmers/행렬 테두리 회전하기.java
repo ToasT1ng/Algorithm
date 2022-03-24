9c070ce83b460666a76d89571fcea42a0b2f3a9a
ToasT1ng <jrainboww9@gmail.com>
2022-03-25 00:35:25 +0900

    Update 행렬 테두리 회전하기.java

--- programmers/행렬 테두리 회전하기.java
+++ programmers/행렬 테두리 회전하기.java
@@ -1,70 +1,84 @@
 import java.util.*;
 
-class Solution {
-    List<Integer> answerList = new ArrayList<>();
-    
-    public int[] solution(int rows, int columns, int[][] queries) {
-        int[][] array = new int[rows][columns];
-        for (int i=0 ; i<rows ; i++) {
-            for (int j=0 ; j<columns ; j++) {
-                array[i][j] = (j+1) + i * columns;
-            }
-        }
-        
-        for (int[] query : queries) {
-            array = doRotate(array, query);
-        }
-        
-        return answerList.stream().mapToInt(i->i).toArray();
+class Coordinate {
+    int x;
+    int y;
+    public Coordinate(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
     
+    public Coordinate toLeft() {
+        return new Coordinate(this.x, this.y-1);
+    }
     
-    // 1. 첫번째 행과 같은 행일 경우, 오른쪽으로 이동하다가 마지막 열과 같은 열일 때 아래로 이동
-    // 2. 마지막 열과 같은 열일 경우, 아래로 이동하다가 마지막 행과 같은 행일때 왼쪽으로 이동
-    // 3. 마지막 행과 같은 행일 경우, 왼쪽으로 이동하다가 첫번째 열과 같은 열일 때 위로 이동
-    // 4. 첫번째 열과 같은 열일 경우, 위로 이동하다가 첫번째 행과 같은 행일때 끝
-    public int[][] doRotate(int[][] array, int[] query) {
-        int firstRow = query[0] - 1; int firstColumn = query[1] - 1;
-        int lastRow = query[2] - 1; int lastColumn = query[3] - 1;
-        int currentRow = firstRow;
-        int currentColumn = firstColumn + 1;
-        int lowValue = array[currentRow][currentColumn];
-        int lastValue = array[currentRow][currentColumn-1];
-        int currentValue = 0;
-        
-        while(true) {
-            currentValue = array[currentRow][currentColumn];
-            array[currentRow][currentColumn] = lastValue;
-            lastValue = currentValue;
-            
-            if (currentValue < lowValue) lowValue = currentValue;
-            
-            if (currentRow == firstRow && currentColumn == firstColumn) {
-                break;
-            }
-            if (currentRow == firstRow) {
-                if (currentColumn < lastColumn) currentColumn++;
-                else currentRow++;
-                continue;
-            }
-            if (currentColumn == lastColumn) {
-                if (currentRow < lastRow) currentRow++;
-                else currentColumn--; 
-                continue;
-            }
-            if (currentRow == lastRow){
-                if (currentColumn > firstColumn) currentColumn--;
-                else currentRow--;
-                continue;
-            }
-            if (currentColumn == firstColumn){
-                if (currentRow > firstRow) currentRow--;
-                else break;
-                continue;
-                
+    public Coordinate toRight() {
+        return new Coordinate(this.x, this.y+1);
+    }
+    
+    public Coordinate toDown() {
+        return new Coordinate(this.x+1, this.y);
+    }
+    
+    public Coordinate toUp() {
+        return new Coordinate(this.x-1, this.y);
+    }
+    
+    public String toString() {
+        return "{x=" + this.x + ",y=" + this.y + "}";
+    }
+}
+
+class Solution {
+    List<Integer> list = new ArrayList<>();
+    
+    public int[] solution(int rows, int columns, int[][] queries) {
+        int[][] matrix = new int[rows][columns];
+        for (int i=0 ; i<rows ; i++) {
+            for (int j=0 ; j<columns ; j++) {
+                matrix[i][j] = i*columns + j + 1;
             }
         }
-        answerList.add(lowValue);
-        return array;
+        
+        for (int[] eachQuery : queries) {
+            rotate(matrix, eachQuery);
+        }
+        
+        return list.stream().mapToInt(i->i).toArray();
+    }
+    
+    public void rotate(int[][] matrix, int[] eachQuery) {
+        Coordinate startCoord = new Coordinate(eachQuery[0]-1, eachQuery[1]-1);
+        Coordinate edgeCoord = new Coordinate(eachQuery[2]-1, eachQuery[3]-1);
+        
+        Coordinate currentCoord = new Coordinate(startCoord.x, startCoord.y);
+        Coordinate beforeCoord = currentCoord.toDown();
+            
+        int currentNum = 0;
+        int beforeNum = matrix[beforeCoord.x][beforeCoord.y];
+        int lowValue = beforeNum;
+        
+        while (true) {
+            currentNum = matrix[currentCoord.x][currentCoord.y];
+            lowValue = Math.min(currentNum, lowValue);
+            matrix[currentCoord.x][currentCoord.y] = beforeNum;
+            beforeNum = currentNum;        
+                
+            if (currentCoord.x == startCoord.x && currentCoord.y < edgeCoord.y) {
+                currentCoord = currentCoord.toRight();
+                    
+            } else if (currentCoord.x < edgeCoord.x && currentCoord.y == edgeCoord.y) {
+                currentCoord = currentCoord.toDown();
+                    
+            } else if (currentCoord.x == edgeCoord.x && currentCoord.y > startCoord.y) {
+                currentCoord = currentCoord.toLeft();
+                    
+            } else {
+                currentCoord = currentCoord.toUp();
+            }
+                
+            if (currentCoord.x == startCoord.x && currentCoord.y == startCoord.y) break;
+        }
+        list.add(lowValue);
     }
 }
