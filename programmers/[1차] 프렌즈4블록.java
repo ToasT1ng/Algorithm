35d199d0cba16002a556b1e8d3de89574ffc6d74
ToasT1ng <jrainboww9@gmail.com>
2022-04-13 22:49:13 +0900

    need to solve test case no.10

--- programmers/[1차] 프렌즈4블록.java
+++ programmers/[1차] 프렌즈4블록.java
@@ -2,123 +2,100 @@
 
 import java.util.*;
 
-class Node {
-    int i;
-    int j;
-    public Node(int i, int j) {
-        this.i = i;
-        this.j = j;
-    }
-    
-    @Override
-    public String toString() {
-        return "{i=" + this.i + ",j=" + this.j + "}";
-    }
-    
-    // public String toSetString() {
-    //     return this.i + "_" + this.j;
-    // }
-    
-    @Override
-    public boolean equals(Object o) {
-        Node node = (Node) o;
-        return (this.i == node.i) && (this.j == node.j);
-    }
-    
-    @Override
-    public int hashCode() {
-        return i * 1000 + j;
-    }
-    
-}
-
 class Solution {
+    int[] nextI = {0,1,1};
+    int[] nextJ = {1,0,1};
+    
     public int solution(int m, int n, String[] board) {
-        int answer = 0;
-        
+        int answer = 0;    
+        boolean[][] visited = new boolean[m][n];
+        char[][] boardChar = new char[m][n];
         for (int i=0 ; i<m ; i++) {
             for (int j=0 ; j<n ; j++) {
-                Set<Node> result = catchBlocks(m,n,board,i,j);
-                answer += result.size();
-                eraseBlocks(result, board);
+                boardChar[i][j] = board[i].charAt(j);
             }
         }
+        while (true) {
+            catchBlocks(visited, boardChar);
+            int tempAns = eraseBlocks(visited, boardChar);
+            answer += tempAns;
+            if (tempAns == 0) break; 
+            
+        }
+        // for (int i=0 ; i<m ; i++) {
+        //     for (int j=0 ; j<n ; j++) {
+        //         System.out.print(boardChar[i][j]);
+        //     }
+        //     System.out.println();
+        // }
         return answer;
     }
     
-    public void eraseBlocks(Set<Node> result, String[] board) {
-        for (Node eachNode : result) {
-            StringBuilder builder = new StringBuilder(board[eachNode.i]);
-            builder.setCharAt(eachNode.j, 'E');
-            board[eachNode.i] = builder.toString();
-        }
-        for (int i=0 ; i<board.length ; i++) {
-            for (int j=0 ; j<board[i].length() ; j++) {
-                if (i != 0 && Character.compare(board[i].charAt(j), 'E') == 0) {
-                    StringBuilder up = new StringBuilder(board[i-1]);
-                    char temp = up.charAt(j);
-                    up.setCharAt(j, 'E');
-                    board[i-1] = up.toString();
-                    
-                    StringBuilder current = new StringBuilder(board[i]);
-                    current.setCharAt(j, temp);
-                    board[i] = current.toString();
+    public void catchBlocks(boolean[][] visited, char[][] boardChar) {
+        for (int i=0 ; i<visited.length ; i++) {
+            for (int j=0 ; j<visited[i].length ; j++) {
+                if (isAbleToGetChar(i,j,visited.length,visited[i].length)) {
+                    char currentChar = boardChar[i][j];
+                    if (Character.compare(currentChar, ' ') == 0) continue;
+                    char right = boardChar[i+nextI[0]][j+nextJ[0]];
+                    char down = boardChar[i+nextI[1]][j+nextJ[1]];
+                    char cross = boardChar[i+nextI[2]][j+nextJ[2]];
+                    if (Character.compare(currentChar, right) == 0 && Character.compare(currentChar, down) == 0 
+                        && Character.compare(currentChar, cross) == 0) {
+                        visited[i][j] = true;
+                        visited[i+nextI[0]][j+nextJ[0]] = true;
+                        visited[i+nextI[1]][j+nextJ[1]] = true;
+                        visited[i+nextI[2]][j+nextJ[2]] = true;
+                    }
                 }
+                    
             }
         }
     }
     
-    public Set<Node> catchBlocks(int m, int n, String[] board, int firstIIndex, int firstJIndex) {
-        
-        boolean[][] visited = new boolean[m][n];
-        
-        Queue<Node> queue = new LinkedList<>();
-        queue.offer(new Node(firstIIndex,firstJIndex));
-        
-        
-        visited[firstIIndex][firstJIndex] = true;
-        
-        int[] nextI = {0,1,1};
-        int[] nextJ = {1,0,1};
-        
-        Set<Node> totalSet = new LinkedHashSet<>();
-        while (!queue.isEmpty()) {
-            Node current = queue.poll();
-            char currentChar = board[current.i].charAt(current.j);
-            if (Character.compare(currentChar, 'E') == 0) {
-                continue;
+    public boolean isAbleToGetChar(int i, int j, int m, int n) {
+        for (int index = 0 ; index<3 ; index++) {
+            if (i+nextI[index] >= 0 && i+nextI[index] < m && j+nextJ[index] >= 0 && j+nextJ[index] < n) continue;
+            else return false;
+        }
+        return true;
+    }
+    
+    public int eraseBlocks(boolean[][] visited, char[][] boardChar) {
+        for (int i=0 ; i<visited.length ; i++) {
+            for (int j=0 ; j<visited[i].length ; j++) {
+                if (visited[i][j]) boardChar[i][j] = ' ';
             }
-            
-            visited[current.i][current.j] = true;
-            
-            Set<Node> set = new LinkedHashSet<>();
-            set.add(current);
-            for (int nextIndex=0 ; nextIndex < nextI.length ; nextIndex++) {
-                int nextIIndex = current.i + nextI[nextIndex];
-                int nextJIndex = current.j + nextJ[nextIndex];
-                if (nextIIndex >= 0 && nextJIndex >= 0 && nextIIndex < m && nextJIndex < n) {
-                    if (!visited[nextIIndex][nextJIndex]) {
-                        char nextChar = board[nextIIndex].charAt(nextJIndex); 
-                        if (Character.compare(currentChar, nextChar) == 0) {
-                            Node next = new Node(nextIIndex, nextJIndex);
-                            queue.offer(next);
-                            visited[nextIIndex][nextJIndex] = true;
-                            set.add(next);
-                        }
+        }
+        
+        int erasingBlocks = 0;
+        for (int i = visited.length-1 ; i>=0 ; i--) {
+            for (int j = visited[i].length-1 ; j>=0 ; j--) {
+                if (visited[i][j]) {
+                    int firstEmpty = i;
+                    int firstFriends = i;
+                    for ( ; firstFriends >= 0 && visited[firstFriends][j] ; firstFriends--) { 
+                        visited[firstFriends][j] = false;
+                        erasingBlocks++;
+                    }
+                    for ( ; firstFriends >= 0 && firstEmpty > firstFriends; firstFriends--, firstEmpty--) {
+                        char friends = boardChar[firstFriends][j];
+                        if (Character.compare(friends, ' ') == 0) firstFriends++;
+                        boardChar[firstFriends][j] = boardChar[firstEmpty][j];
+                        boardChar[firstEmpty][j] = friends;
                     }
                 }
             }
-            if (set.size() >= 4) totalSet.addAll(set);
-            for (Node eachNode : set) {
-                visited[eachNode.i][eachNode.j] = false;
-            }
         }
-        
-        for (Node eachNode : totalSet) {
-            visited[eachNode.i][eachNode.j] = true;
-        }
-        // System.out.println(Integer.toString(firstIIndex) + " " + Integer.toString(firstJIndex) + totalSet.toString());
-        
-        return totalSet;
+        return erasingBlocks;
     }
+    
+    // AA
+    // A
+    // CC
+    // A
+    // A
+    // DC
+    
+    
 }
