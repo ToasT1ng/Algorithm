d05fb7a04f0b25f75d974b2d427bb75c073759ce
ToasT1ng <jrainboww9@gmail.com>
2022-05-09 22:42:58 +0900

    Update 단체사진 찍기.java

--- programmers/단체사진 찍기.java
+++ programmers/단체사진 찍기.java
@@ -47,3 +47,45 @@ class Solution {
         return true;
     }
 }
+
+
+// 22 05 09
+// class Solution {
+// 	int answer = 0;
+//     String[] friends = {"A", "C", "F", "J", "M", "N", "R", "T"};
+// 	public int solution(int n, String[] data) {  
+// 		dfs(0, data, new boolean[8], "");
+// 		return answer;
+// 	}
+
+// 	public void dfs(int pos, String[] data, boolean[] visited, String result) {
+// 		if (result.length() >= 8) {
+// 			if (isCorrect(data, result)) answer+=1;
+// 		}
+// 		for (int i=0 ; i<8 ; i++) {
+// 			if (!visited[i]) {
+// 				visited[i] = true;
+// 				result += friends[i];
+// 				dfs(pos+1, data, visited, result);
+// 				result = result.substring(0,result.length()-1);
+// 				visited[i] = false;
+// 			}
+// 		}
+// 	}
+
+// 	public boolean isCorrect(String[] data, String result) {
+// 		for (String each : data) {
+// 			int distance = Math.abs(result.indexOf(each.charAt(0)) - result.indexOf(each.charAt(2))) - 1;
+// 			String comparator = String.valueOf(each.charAt(3));
+// 			int compareValue = Integer.parseInt(String.valueOf(each.charAt(4)));
+// 			if (comparator.equals("=")) {
+// 				if (distance != compareValue) return false;
+// 			} else if (comparator.equals(">")) {
+// 				if (distance <= compareValue) return false;
+// 			} else {
+// 				if (distance >= compareValue) return false;
+// 			}
+// 		}
+// 		return true;
+// 	}
+// }
