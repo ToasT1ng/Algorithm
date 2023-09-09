de665b3b7483bcf9746511e0d5c8679abe089f06
ToasT1ng <jrainboww9@gmail.com>
2023-09-09 21:51:35 +0900

    Update 단어 변환.java

--- programmers/단어 변환.java
+++ programmers/단어 변환.java
@@ -1,47 +1,39 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/43163
 
 class Solution {
-    int answer = 100;
+    int answer = 0;
     
     public int solution(String begin, String target, String[] words) {
-        boolean[] visited = new boolean[words.length];
-        dfs(0, 0, begin, target, words, visited);
+        answer = words.length+1;
         
-        return answer == 100 ? 0 : answer;
+        dfs(0, begin, 0, new boolean[words.length], target, words);
+        
+        return answer == words.length+1 ? 0 : answer;
     }
     
-    public void dfs(int index, int number, String current, String target, String[] words, boolean[] visited) {
+    public void dfs(int index, String current, int count, boolean[] visited, String target, String[] words) {
         if (current.equals(target)) {
-            answer = Math.min(answer, number);
+            answer = Math.min(answer, count);
             return;
         }
         
-        if (index > words.length-1) {
-            return;
-        }
+        if (index >= words.length) return;
         
         for (int i=0 ; i<words.length ; i++) {
-            if (!visited[i] && checkAbleToChange(current, words[i])) {
-                visited[i] = true;
-                
-                dfs(i, number+1, words[i], target, words, visited);
-                
-                visited[i] = false;
-            }
+            if (visited[i]) continue;
+            
+            visited[i] = true;
+            if (isChangable(current, words[i])) dfs(index+1, words[i], count+1, visited, target, words);
+            visited[i] = false;
         }
     }
     
-    public boolean checkAbleToChange(String currentString, String changeString) {
+    public boolean isChangable(String input, String target) {
         int count = 0;
-        for (int i=0 ; i<currentString.length() ; i++) {
-            if (currentString.charAt(i) != changeString.charAt(i)) {
-                count++;
-            }
-            if (count >= 2) {
-                return false;
-            }
+        for (int i=0 ; i<input.length() ; i++) {
+            if (input.charAt(i) != target.charAt(i)) count++;
+            if (count >= 2) return false;
         }
         return true;
-        
     }
 }
