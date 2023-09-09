f3ed70bc64190aea14b1d9f851e635d763d24398
ToasT1ng <jrainboww9@gmail.com>
2023-09-09 22:23:57 +0900

    Update 네트워크.java

--- programmers/네트워크.java
+++ programmers/네트워크.java
@@ -1,29 +1,28 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/43162#
 
 class Solution {
-    int answer = 0;
-    
-    public int solution(int n, int[][] computers) {    
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        
         boolean[] visited = new boolean[n];
         
         for (int i=0 ; i<n ; i++) {
-            visited[i] = false;
-        }
-        
-        for (int i=0 ; i<n ; i++) {
             if (!visited[i]) {
-                answer++;
                 visited[i] = true;
+                answer++;
                 dfs(i, computers, visited);
             }
+            
         }
         
         return answer;
     }
     
-    public void dfs(int currentComputer, int[][] computers, boolean[] visited) {
-        for (int i=0 ; i<computers.length ; i++) {
-            if (!visited[i] && computers[currentComputer][i] == 1) {
+    public void dfs(int index, int[][] computers, boolean[] visited) {
+        for (int i=0 ; i<visited.length ; i++) {
+            if (visited[i]) continue;
+            
+            if (computers[i][index] == 1) {
                 visited[i] = true;
                 dfs(i, computers, visited);
             }
